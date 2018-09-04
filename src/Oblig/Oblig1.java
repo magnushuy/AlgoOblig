37e06f323af6cba40c431045ca9233be2a00add5
PiotrKusnierz <piotr.kusnierz@interia.eu>
2018-09-04 13:04:15 +0200

    Oppgave 4 delvis ferdi(må gjøres alt om til en metode)

--- src/Oblig/Oblig1.java
+++ src/Oblig/Oblig1.java
@@ -28,7 +28,7 @@ public class Oblig1 {
         //Oppgave 4
         int[] a = {6,10,9,4,1,3,8,5,2,7};
         delsortering(a);
-        System.out.println(Arrays.toString(a));
+        System.out.println("Oppgave 4: " +Arrays.toString(a));
     }
 
     //Start av maks metoden som finner største tall i en array og returnerer den
@@ -130,29 +130,45 @@ public class Oblig1 {
         return verdi;
     }
 
+
     //oppgave 4
+    static void delsortering(int a[]) {
+        int evenPnt = 0;
 
-    public static void delsortering(int[]a){
-        int v = 0;
-        int h = a.length-1;
-
-
-        for(int i = 0; i < a.length; i++) {
-            if(a[v] % 2 == 1) {
-                v++;
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] % 2 == 0) {
+                swap(a, evenPnt, i);
+                evenPnt++;
             }
-
-            if(a[h] % 2 == 0) {
-                h--;
-            }
-
-            if (v < h) {
-                int temp = a[v];
-                a[v] = a[h];
-                a[h] = temp;
-            }
-
         }
+
+        // Sorting even numbers
+        sort(a, 0, evenPnt);
+
+        // Sorting Odd numbers
+        sort(a, evenPnt, a.length);
+
+        for (int i : a) {
+            System.out.print(i+", ");
+        }
+    }
+
+    public static void sort(int[] arr, int min, int max) {
+        for (int i = min; i < (max - 1); i++) {
+            for (int j = i + 1; j < max; j++) {
+                if (arr[i] > arr[j]) {
+                    swap(arr, i, j);
+                }
+            }
+        }
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+
 
     //oppgave 5 og 6
     public static void rotasjon(char[] c, int k){
