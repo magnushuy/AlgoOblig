b49459c5eb93d34c283c95fd5c75b14ce9d4ed65
PiotrKusnierz <piotr.kusnierz@interia.eu>
2018-09-04 13:34:46 +0200

    Løst oppgave 4, men kanskje de finnes mer consist løsning enn det her.

--- src/Oblig/Oblig1.java
+++ src/Oblig/Oblig1.java
@@ -132,42 +132,29 @@ public class Oblig1 {
 
 
     //oppgave 4
-    static void delsortering(int a[]) {
+    static void delsortering(int[] a) {
         int evenPnt = 0;
 
+        // Arranging Even and Odd numbers
         for (int i = 0; i < a.length; i++) {
-            if (a[i] % 2 == 0) {
-                swap(a, evenPnt, i);
+            if (a[i] % 2 == 1) {
+                bytt(a, evenPnt, i);
                 evenPnt++;
             }
         }
 
         // Sorting even numbers
-        sort(a, 0, evenPnt);
+        sorter(a, 0, evenPnt);
 
         // Sorting Odd numbers
-        sort(a, evenPnt, a.length);
+        sorter(a, evenPnt, a.length);
 
         for (int i : a) {
             System.out.print(i+", ");
         }
     }
 
-    public static void sort(int[] arr, int min, int max) {
-        for (int i = min; i < (max - 1); i++) {
-            for (int j = i + 1; j < max; j++) {
-                if (arr[i] > arr[j]) {
-                    swap(arr, i, j);
-                }
-            }
-        }
-    }
 
-    public static void swap(int[] arr, int i, int j) {
-        int tmp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = tmp;
-    }
 
 
     //oppgave 5 og 6
@@ -203,6 +190,29 @@ public class Oblig1 {
 
         System.out.println("Tabell etter sortering: " + Arrays.toString(c));
     }
+
+
+
+
+     /*
+        METODER FRA UNDERVISNINGEN OG HJELPEMETODER
+     */
+     public static void sorter(int[] a, int min, int max) {
+         for (int i = min; i < (max - 1); i++) {
+             for (int j = i + 1; j < max; j++) {
+                 if (a[i] > a[j]) {
+                     bytt(a, i, j);
+                 }
+             }
+         }
+     }
+
+     public static void bytt(int[] a, int i, int j) {
+         int temp = a[i];
+         a[i] = a[j];
+         a[j] = temp;
+     }
+
 
 
 
